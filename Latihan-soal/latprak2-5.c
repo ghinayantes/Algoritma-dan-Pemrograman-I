f1619055eb4237344fd79176c5007660477a43c9
fromdearest <ghinayantes2006@gmail.com>
2026-03-09 23:25:42 +0700

    latsol subprogram

--- Latihan-soal/latprak2-5.c
+++ Latihan-soal/latprak2-5.c
@@ -1,5 +1,59 @@
 #include <stdio.h>
 
 int isKabisat(int yy) {
-    if((yy mod 4 == 0) && (yy mod 100 != 0)) || 
+    if(((yy % 4 == 0) && (yy % 100 != 0)) || (yy % 400 == 0)) return 1;
+    else return 0;
 }
+
+int dayMax(int mm, int yy) {
+    if(isKabisat(yy)) {
+        if (mm == 1 || mm == 3 || mm == 5 || mm == 7 || mm == 8 || mm == 10 || mm == 12) return 31;
+        else if (mm == 2) return 29;
+        else return 30;
+    }
+    else {
+        if (mm == 4 || mm == 6 || mm == 9 || mm == 11) return 30;
+        else if (mm == 2) return 28;
+        else return 31;
+    }
+}
+
+int isTanggalValid(int dd, int mm, int yy) {
+    if ((dd > 0 && dd <= dayMax(mm, yy)) && (mm > 0 && mm <= 12)) return 1;
+    else return 0;
+}
+
+void bacaTanggal(int *dd, int *mm, int *yy) {
+    do {
+        printf("Masukkan tanggal sekarang (dd mm yy): ");
+        scanf("%d %d %d", dd, mm, yy);
+    } while(!isTanggalValid(*dd, *mm, *yy));
+}
+
+void nextDay(int *dd, int *mm, int *yy) {
+    if (*dd < dayMax(*mm, *yy)) {
+        *dd += 1;
+    } else {
+        *dd = 1;
+        if (*mm < 12) {
+            *mm += 1;
+        } else {
+            *mm = 1;
+            *yy += 1;
+        }
+    }
+}
+
+int main() {
+    int dd, mm, yy;
+
+    bacaTanggal(&dd, &mm, &yy);
+    nextDay(&dd, &mm, &yy);
+
+    printf("Keesokan harinya: %d %d %d\n", dd, mm, yy);
+
+    return 0;
+}
+
+    
+
