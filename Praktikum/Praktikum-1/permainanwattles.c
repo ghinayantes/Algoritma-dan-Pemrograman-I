59a587144660ab2aeb0fd12d7d80f200c33dccae
fromdearest <ghinayantes2006@gmail.com>
2026-03-12 17:53:53 +0700

    ralat

--- Praktikum/Praktikum-1/permainanwattles.c
+++ Praktikum/Praktikum-1/permainanwattles.c
@@ -1,36 +1,41 @@
 #include <stdio.h>
 
+int digitPertama(int n) {
+    while(n >= 10) n /= 10;
+    return n;
+}
+
+int digitTerakhir(int n) {
+    return n % 10;
+}
+
 int main() {
     int deep, meb;
-    int m = 0, d = 0;
-
-    scanf("%d %d",&deep, &meb);
-    int copd = deep;
-    int copmeb = meb;
+    scanf("%d %d", &deep, &meb);
 
     while(deep >= 0 && meb >= 0) {
-
-        m += copmeb%10;
-        while(copmeb >= 10) {
-            copmeb/=10;
-    }
-        d += copd%10;
-        while(copd >= 10) {
-            copd/=10;
-    }
-        deep = deep - m;
-        meb = meb - d;
-    }
-
-    if (meb > deep) {
-            printf("Mebel\n");
-        }
-        else if (deep > meb) {
-            printf("Deeper\n");
-        }
-        else {
+        // cek kondisi Wattles: kedua bilangan sama
+        if(deep == meb) {
             printf("Wattles\n");
+            return 0;
         }
+
+        // hitung pengurangan berdasarkan bilangan lawan turn sebelumnya
+        int redDeep = digitPertama(meb) + digitTerakhir(meb);
+        int redMeb  = digitPertama(deep) + digitTerakhir(deep);
+
+        deep = deep - redDeep;
+        meb  = meb  - redMeb;
+
+        // cek kondisi Wattles: keduanya negatif di turn yang sama
+        if(deep < 0 && meb < 0) {
+            printf("Wattles\n");
+            return 0;
+        }
+    }
+
+    if(deep < 0) printf("Mebel\n");
+    else printf("Deeper\n");
 
     return 0;
 }
